4511cc54df500abd6a396f910612b3cd044471f2
chrisarmstrong <chris@niice.co>
2023-04-15 04:41:22 +0100

    Initial styles

--- styles/globalStyles.js
+++ styles/globalStyles.js
@@ -1,11 +1,91 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  body {
+
+@font-face {
+  font-family: "Family";
+  src: url("/fonts/family-light.woff2") format("woff2");
+  font-weight: 300;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Family";
+  src: url("/fonts/family-light-italic.woff2") format("woff2");
+  font-weight: 300;
+  font-style: italic;
+}
+
+@font-face {
+  font-family: "Family";
+  src: url("/fonts/family-regular.woff2") format("woff2");
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Family";
+  src: url("/fonts/family-regular-italic.woff2") format("woff2");
+  font-weight: normal;
+  font-style: italic;
+}
+
+@font-face {
+  font-family: "Family";
+  src: url("/fonts/family-bold.woff2") format("woff2");
+  font-weight: bold;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Family";
+  src: url("/fonts/family-bold-italic.woff2") format("woff2");
+  font-weight: bold;
+  font-style: italic;
+}
+
+
+  /* Box sizing rules */
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  /* Remove default margin */
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  ol,
+  ul {
     margin: 0;
+  }
+
+  /* Remove default padding */
+  body,
+  p {
     padding: 0;
-    background: teal;
-    font-family: Open-Sans, Helvetica, Sans-Serif;
+  }
+
+  /* Remove list styles on ul, ol elements */
+  ol,
+  ul {
+    list-style: none;
+  }
+
+  /* Set default font size */
+  html {
+    font-size: 16px;
+  }
+
+  /* Reset default font family */
+  body {
+    font-family: Family, georgia, serif;
   }
 `;
 
