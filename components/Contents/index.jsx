1dee87beb1a37bfef20142b51a155860f1908823
chrisarmstrong <chris@niice.co>
2023-04-18 01:20:09 +0100

    try removing / from url

--- components/Contents/index.jsx
+++ components/Contents/index.jsx
@@ -70,7 +70,7 @@ export default function Contents({ active, dismiss, books, goToPosition }) {
 				{books.map((book, i) => (
 					<Link
 						key={book.book}
-						href={"/" + book.book.toLowerCase().replace(/\s+/g, "-")}
+						href={book.book.toLowerCase().replace(/\s+/g, "-")}
 						target="_self"
 						data-index={i}
 						onClick={() => {
