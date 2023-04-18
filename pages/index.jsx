fbf0feacc0c20ba99d9a01c21abc061d308db79e
chrisarmstrong <chris@niice.co>
2023-04-18 01:14:38 +0100

    Add meta tags

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,13 +13,22 @@ export default function Home() {
 					name="viewport"
 					content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
 				/>
+				<meta name="application-name" content="Bible" />
+				<meta name="msapplication-starturl" content="https://simplebible.app" />
+				<meta name="mobile-web-app-capable" content="yes" />
 				<meta name="apple-mobile-web-app-capable" content="yes" />
+				<meta name="apple-mobile-web-app-title" content="Bible" />
 				<link
 					rel="apple-touch-icon"
 					sizes="180x180"
 					href="/apple-touch-icon.png"
 				></link>
 				<link rel="icon" href="/apple-touch-icon.png" />
+				<link
+					rel="manifest"
+					crossOrigin="use-credentials"
+					href="/manifest.json"
+				/>
 			</Head>
 
 			<Main />
