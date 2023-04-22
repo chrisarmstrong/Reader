2ad7fc1ec58d320bb753bdac1f8a0e1dea595842
chrisarmstrong <chris@niice.co>
2023-04-22 10:54:25 +0100

    Move <Head> declarations to Main

--- components/Main/index.jsx
+++ components/Main/index.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import GlobalStyle from "../../styles/globalStyles";
+import Head from "next/head";
 
 import { useState, useEffect, useRef } from "react";
 
@@ -56,6 +57,30 @@ export default function Main({ slug, book }) {
 
 	return (
 		<>
+			<Head>
+				<title>Bible</title>
+				<meta name="description" content="A simple Bible app" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
+				/>
+				<meta name="application-name" content="Bible" />
+				<meta name="msapplication-starturl" content="https://simplebible.app" />
+				<meta name="mobile-web-app-capable" content="yes" />
+				<meta name="apple-mobile-web-app-capable" content="yes" />
+				<meta name="apple-mobile-web-app-title" content="Bible" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				></link>
+				<link rel="icon" href="/apple-touch-icon.png" />
+				<link
+					rel="manifest"
+					crossOrigin="use-credentials"
+					href="/manifest.json"
+				/>
+			</Head>
 			<GlobalStyle />
 
 			<Container>
--- pages/index.jsx
+++ pages/index.jsx
@@ -6,31 +6,6 @@ import Main from "../components/Main";
 export default function Home() {
 	return (
 		<>
-			<Head>
-				<title>Bible</title>
-				<meta name="description" content="A simple Bible app" />
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
-				/>
-				<meta name="application-name" content="Bible" />
-				<meta name="msapplication-starturl" content="https://simplebible.app" />
-				<meta name="mobile-web-app-capable" content="yes" />
-				<meta name="apple-mobile-web-app-capable" content="yes" />
-				<meta name="apple-mobile-web-app-title" content="Bible" />
-				<link
-					rel="apple-touch-icon"
-					sizes="180x180"
-					href="/apple-touch-icon.png"
-				></link>
-				<link rel="icon" href="/apple-touch-icon.png" />
-				<link
-					rel="manifest"
-					crossOrigin="use-credentials"
-					href="/manifest.json"
-				/>
-			</Head>
-
 			<Main />
 		</>
 	);
