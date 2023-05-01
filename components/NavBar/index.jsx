6e6a4deb08997efafcb603f227896d28a97e91e3
chrisarmstrong <chris@niice.co>
2023-05-02 00:48:31 +0100

    add a little more padding

--- components/NavBar/index.jsx
+++ components/NavBar/index.jsx
@@ -6,7 +6,7 @@ import iconMenu from "./images/icon-menu.svg";
 const Container = styled.div`
 	width: 100%;
 	position: fixed;
-	padding-bottom: calc(env(safe-area-inset-bottom) - 24px);
+	padding-bottom: calc(env(safe-area-inset-bottom) - 12px);
 
 	left: 0;
 	right: 0;
